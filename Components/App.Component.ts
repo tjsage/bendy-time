31d48eb88eefca457a6d549afd2f044b519fca3b
Travis Sage <travis.j.sage@gmail.com>
2016-05-26 21:38:08 -0600

    First commit

---
+++ Components/App.Component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'time-catcher',
+    template: '<h1>Time Catcher has been bootstrapped.</h1>'
+})
+export class AppComponent { }
---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var App_Component_1 = require('./Components/App.Component');
+platform_browser_dynamic_1.bootstrap(App_Component_1.AppComponent);
+//# sourceMappingURL=main.js.map
